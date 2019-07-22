1c531de5f5935e5d48263d0adf086ab208f80bbe
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:45:12 +0200

    :bookmark: v2.0.14
    Dependency updates

--- dist/enumify-x.js
+++ dist/enumify-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-21T23:47:22.951Z",
+  "date": "2019-07-22T13:45:01.505Z",
   "describe": "",
   "description": "Enumerated type library.",
   "file": "enumify-x.js",
   "hash": "dae7d572c98476827995",
   "license": "MIT",
-  "version": "2.0.13"
+  "version": "2.0.14"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
